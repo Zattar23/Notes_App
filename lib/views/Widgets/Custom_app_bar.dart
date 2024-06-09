4e3e038c23afeeee43fbb107e59a4797ed903c06
Abdullah Zattar <zateer549.gmail.com>
2024-06-09 12:50:56 +0300

    add cusrom app bar

--- lib/views/Widgets/Custom_app_bar.dart
+++ lib/views/Widgets/Custom_app_bar.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notsapp/views/Widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.Title, required this.icon});
+
+  final String Title;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
+      children: [
         Text(
-          'Notes',
+          Title,
           style: TextStyle(fontSize: 28),
         ),
         Spacer(),
-        const CustomSearchIcon(),
+        CustomSearchIcon(
+          icon: icon,
+        ),
       ],
     );
   }
--- lib/views/Widgets/Eidt_ViewBody.dart
+++ lib/views/Widgets/Eidt_ViewBody.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:notsapp/views/Widgets/Custom_app_bar.dart';
 
 class EditViewBody extends StatelessWidget {
   const EditViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            Title: 'Edit Note',
+            icon: Icons.check,
+          )
+        ],
+      ),
+    );
   }
 }
