0ae4ce601fff8c0933172014975b8bfaa32dae7c
Abdullah Zattar <zateer549.gmail.com>
2024-06-09 13:13:21 +0300

    ui done

--- lib/views/Widgets/Eidt_ViewBody.dart
+++ lib/views/Widgets/Eidt_ViewBody.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notsapp/views/Widgets/Custom_app_bar.dart';
+import 'package:notsapp/views/Widgets/custom_text_field.dart';
 
 class EditViewBody extends StatelessWidget {
   const EditViewBody({super.key});
@@ -8,7 +9,7 @@ class EditViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
+      child: const Column(
         children: [
           SizedBox(
             height: 50,
@@ -16,7 +17,18 @@ class EditViewBody extends StatelessWidget {
           CustomAppBar(
             Title: 'Edit Note',
             icon: Icons.check,
-          )
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: 'title'),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'content',
+            maxLines: 5,
+          ),
         ],
       ),
     );
