da41fedb7308d233812c3f28a06ed1bb146413a3
Abdullah Zattar <zateer549.gmail.com>
2024-06-09 12:05:34 +0300

    add two text field

--- lib/views/Widgets/custom_text_field.dart
+++ lib/views/Widgets/custom_text_field.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:notsapp/color.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  final String hint;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kprimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(color: kprimaryColor),
